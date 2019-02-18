c1dad9c394ec8f20dba05cc1bf53474c472cdcce
Alec Lomas <alec@lowmess.com>
2019-02-17 20:54:30 -0700

    :ambulance: fix typo in react config

--- react.js
+++ react.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: [
     './lib/rules.js',
     'plugin:react/recommended',
-    'plugin:jsx-a11y:recommended',
+    'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
   ],
   plugins: ['react', 'jsx-a11y', 'prettier'],
