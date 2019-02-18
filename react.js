8393b0dd0e689c2d1e4f282f154505e7c8b2d0eb
Alec Lomas <alec@lowmess.com>
2019-02-17 20:49:21 -0700

    :sparkles: use jsx-a11y plugin in react config

--- react.js
+++ react.js
@@ -2,9 +2,10 @@ module.exports = {
   extends: [
     './lib/rules.js',
     'plugin:react/recommended',
+    'plugin:jsx-a11y:recommended',
     'plugin:prettier/recommended',
   ],
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'jsx-a11y', 'prettier'],
   parserOptions: {
     exmaFeatures: {
       jsx: true,
