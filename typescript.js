1b1f8cbab2c32b2040d1433667c5ed11d8d28156
Alec Lomas <alec@lowmess.com>
2021-04-13 16:17:41 -0700

    :alien: remove explicit prettier/typescript extension
    it's been merged into the main branch

--- typescript.js
+++ typescript.js
@@ -1,28 +1,27 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    warnOnUnsupportedTypeScriptVersion: true,
-  },
-  env: {
-    es6: true,
-  },
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-    'import/resolver': {
-      typescript: {},
-    },
-  },
-  extends: [
-    './lib/rules.js',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/warnings',
-    'plugin:import/errors',
-    'plugin:import/typescript',
-    'prettier',
-    'prettier/@typescript-eslint',
-  ],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+		warnOnUnsupportedTypeScriptVersion: true,
+	},
+	env: {
+		es6: true,
+	},
+	settings: {
+		'import/parsers': {
+			'@typescript-eslint/parser': ['.ts', '.tsx'],
+		},
+		'import/resolver': {
+			typescript: {},
+		},
+	},
+	extends: [
+		'./lib/rules.js',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:import/warnings',
+		'plugin:import/errors',
+		'plugin:import/typescript',
+		'prettier',
+	],
 }
