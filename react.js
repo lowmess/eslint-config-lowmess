52ad8324aa2255446a39dbaa63433016c56ba869
Alec Lomas <alec@lowmess.com>
2021-04-13 16:08:50 -0700

    :alien: remove explicit prettier/react extension
    it was merged into the main ruleset

--- react.js
+++ react.js
@@ -1,38 +1,37 @@
 module.exports = {
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    exmaFeatures: {
-      jsx: true,
-    },
-  },
-  env: {
-    es6: true,
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx'],
-      },
-    },
-  },
-  plugins: ['react-hooks'],
-  extends: [
-    './lib/rules.js',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:import/warnings',
-    'plugin:import/errors',
-    'plugin:import/react',
-    'prettier',
-    'prettier/react',
-  ],
-  rules: {
-    'react-hooks/rules-of-hooks': 2,
-    'react-hooks/exhaustive-deps': 1,
-  },
+	parser: 'babel-eslint',
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+		exmaFeatures: {
+			jsx: true,
+		},
+	},
+	env: {
+		es6: true,
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+		'import/resolver': {
+			node: {
+				extensions: ['.js', '.jsx'],
+			},
+		},
+	},
+	plugins: ['react-hooks'],
+	extends: [
+		'./lib/rules.js',
+		'plugin:react/recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:import/warnings',
+		'plugin:import/errors',
+		'plugin:import/react',
+		'prettier',
+	],
+	rules: {
+		'react-hooks/rules-of-hooks': 2,
+		'react-hooks/exhaustive-deps': 1,
+	},
 }
